723db45ad57391ef87a4483f339a02a876571899
yeggrrr <yjkim981103@gmail.com>
2024-05-07 19:57:11 +0900

    [Add] UISearchBar 추가

--- yegrSearchBookApp/yegrSearchBookApp/SearchViewController.swift
+++ yegrSearchBookApp/yegrSearchBookApp/SearchViewController.swift
@@ -9,8 +9,35 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    let searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray
+        setupSearchBar()
+    }
+    
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func setupSearchBar() {
+        searchBar.searchBarStyle = UISearchBar.Style.default
+        searchBar.placeholder = "검색"
+        searchBar.delegate = self
+        
+        // 텍스트필드 색상 변경
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+            textField.textColor = .black
+            textField.backgroundColor = .white
+        }
+        
+        // 플레이스홀더 컬러
+        let textFieldInsideSearchBar = searchBar.value(forKey: "searchField") as? UITextField
+        textFieldInsideSearchBar?.attributedPlaceholder = NSAttributedString(string: "책 이름을 입력해주세요.", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+        
+        // 네비게이션 바에 넣기
+        navigationItem.titleView = searchBar
     }
 }
